c20f9eece5365389b06c41863843e4dad616bb43
Alejandro <Alejandro-Perezg>
2019-10-04 16:19:48 -0500

    Initial commit

---
+++ desktop/src/mx/apgonza/pruebabox2d/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package mx.apgonza.pruebabox2d.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import mx.apgonza.pruebabox2d.Juego;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Juego(), config);
+	}
+}
